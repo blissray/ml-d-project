1b317f42dfd56854a7b61135d63a040281a71208
Sungchul Choi <sc82.choi@gachon.ac.kr>
2019-11-29 09:27:11 +0900

    Fix bugs

--- code/dl/cnn/dog_breeds/train.py
+++ code/dl/cnn/dog_breeds/train.py
@@ -11,13 +11,16 @@ def _make_generator(train, dataset_path,
                     target_size = (224,224), batch_size=32):
     if train:
         datagen = ImageDataGenerator(
-                        rescale=1. / 255)
+                    shear_range=0.2,
+                    zoom_range=0.2,
+                    horizontal_flip=True,
+                    rescale=1. / 255)
     else:
         datagen = ImageDataGenerator(rescale=1. / 255)
-    datagen.flow_from_directory(
+    generator = datagen.flow_from_directory(
             dataset_path,
-            target_size=target_size, class_mode='categorical', batch_size=32)
-    return datagen 
+            target_size=target_size, class_mode='sparse', batch_size=32, shuffle=True)
+    return generator 
 
 def main(args):
     model = ResNet50ForDogBreed()
@@ -42,12 +45,17 @@ def main(args):
 
     train_generator = _make_generator(train=True, dataset_path=train_path)
     val_generator = _make_generator(train=False, dataset_path=val_path)
+   
+    STEP_SIZE_TRAIN= 30802 // 32
+    STEP_SIZE_VALID= 8058 // 32 
+
 
     model.fit_generator(
         train_generator,
-        steps_per_epoch=2000,
+        steps_per_epoch=STEP_SIZE_TRAIN,
         epochs=epochs,
-        validation_data=val_generator)
+        validation_data=val_generator,
+        validation_steps=STEP_SIZE_VALID)
     model.save_weights('first_try.h5')  # always save your weights after training or during training
 
 if __name__ == '__main__':
