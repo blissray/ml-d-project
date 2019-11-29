5309b197beaf66e11efff042dc410f9c7033ed4c
Sungchul Choi <sc82.choi@gachon.ac.kr>
2019-11-29 19:20:57 +0900

    Update train.py for dog_breed

--- code/dl/cnn/dog_breeds/train.py
+++ code/dl/cnn/dog_breeds/train.py
@@ -8,33 +8,37 @@ from absl import app
 from absl import flags
 
 def _make_generator(train, dataset_path, 
-                    target_size = (224,224), batch_size=32):
+                    target_size = (224,224), batch_size=512):
     if train:
         datagen = ImageDataGenerator(
                     shear_range=0.2,
                     zoom_range=0.2,
-                    horizontal_flip=True,
-                    rescale=1. / 255)
+                    horizontal_flip=True)
     else:
-        datagen = ImageDataGenerator(rescale=1. / 255)
+        datagen = ImageDataGenerator()
     generator = datagen.flow_from_directory(
             dataset_path,
             target_size=target_size, class_mode='sparse', batch_size=32, shuffle=True)
     return generator 
 
-def main(args):
-    model = ResNet50ForDogBreed()
-
-    loss_object = losses.SparseCategoricalCrossentropy()
-    learning_rate = 0.5
+def _make_optimizer():
+    learning_rate = 0.01
     beta_1 = 0.9
     beta_2 = 0.99
-    epsilon = 0.00001
-    epochs = 100
-
+    epsilon = 0.0000001
     optimizer = optimizers.Adam(learning_rate, beta_1, beta_2, epsilon)
+    return optimizer
 
-    model.compile(loss=loss_object,
+def _number_of_files(dir_path):
+    file_counter = 0
+    for dir_name in os.listdir(dir_path):
+        file_counter += len(list(os.listdir(os.path.join(dir_path, dir_name))))
+    return file_counter
+
+def main(args):
+    model = ResNet50ForDogBreed(input_shape=(224,224,3))
+    optimizer = _make_optimizer()
+    model.compile(loss="sparse_categorical_crossentropy",
               optimizer=optimizer,
               metrics=['accuracy'])
     DATA_DIR = "data"
@@ -42,12 +46,18 @@ def main(args):
     VAL = "val"
     train_path = os.path.join(DATA_DIR, TRAIN) 
     val_path = os.path.join(DATA_DIR, VAL) 
+    
+    number_of_train_files = _number_of_files(train_path)
+    number_of_val_files = _number_of_files(val_path)
+    
+    batch_size =512
+    epochs = 500
 
-    train_generator = _make_generator(train=True, dataset_path=train_path)
-    val_generator = _make_generator(train=False, dataset_path=val_path)
-   
-    STEP_SIZE_TRAIN= 30802 // 32
-    STEP_SIZE_VALID= 8058 // 32 
+    train_generator = _make_generator(train=True, dataset_path=train_path, target_size = (224,224), batch_size=batch_size)
+    val_generator = _make_generator(train=False, dataset_path=val_path, batch_size=batch_size)
+
+    STEP_SIZE_TRAIN= number_of_train_files // batch_size
+    STEP_SIZE_VALID= number_of_val_files // batch_size
 
 
     model.fit_generator(
@@ -59,7 +69,7 @@ def main(args):
     model.save_weights('first_try.h5')  # always save your weights after training or during training
 
 if __name__ == '__main__':
-  app.run(main)
+    app.run(main)
 
 
 
