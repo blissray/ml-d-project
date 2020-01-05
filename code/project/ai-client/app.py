59c14ab63a2a0dcfe5634700ff2a40420a08cdff
Sungchul Choi <blissray@gmail.com>
2020-01-06 00:38:06 +0900

    add ai-client

--- code/project/ai-client/app.py
+++ code/project/ai-client/app.py
@@ -4,6 +4,8 @@
 import os
 from datetime import datetime
 from flask import Flask, render_template, jsonify, redirect, url_for, request
+import requests
+import json
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -23,10 +25,19 @@ def upload():
             now = datetime.now()
             filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), file.filename.rsplit('.', 1)[1]))
             file.save(filename)
-            return jsonify({"breed":"NAME", "prob":"0.97"})
+
+            url = 'http://estella.gachon.ac.kr:8000/predict'
+            files = {'file': open(filename, 'rb')}
+
+            r = requests.post(url, files=files)
+            data = json.loads(r.text)
+
+            return jsonify(data)
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 if __name__ == "__main__":
     app.run(debug=True)
