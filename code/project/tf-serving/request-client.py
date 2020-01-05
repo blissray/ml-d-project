b0e41101edef055d78a8df11472409745cc614e6
Sungchul Choi <blissray@gmail.com>
2020-01-06 00:39:13 +0900

    Update request-client.py

--- code/project/tf-serving/request-client.py
+++ code/project/tf-serving/request-client.py
@@ -1,7 +1,11 @@
 import requests
+import json
 
 url = 'http://estella.gachon.ac.kr:8000/predict'
 files = {'file': open('test.jpg', 'rb')}
 
 r = requests.post(url, files=files)
-print(r.text["result"])
+data = json.loads(r.text)
+
+print(data["breed"])
+print(data["prob"])
