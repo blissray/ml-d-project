be41bfa0ae595be9fbff7efe867f5e580f2ab46e
Sungchul Choi <blissray@gmail.com>
2020-01-05 23:06:20 +0900

    update serving app.y

--- code/project/tf-serving/app.py
+++ code/project/tf-serving/app.py
@@ -1,6 +1,14 @@
-# load Flask 
+# load Flask
 import flask
-app = flask.Flask(__name__)
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import url_for
+from flask import request
+
+from datetime import datetime
+
+app = Flask(__name__)
 
 import tempfile
 import tensorflow as tf
@@ -13,25 +21,33 @@ CHECKPOINT_DIR = "checkpoints"
 model_list = sorted([os.path.join(CHECKPOINT_DIR, filename)
     for filename in os.listdir(CHECKPOINT_DIR)])
 
-
 new_model = keras.models.load_model(model_list[-1])
 print(new_model.summary())
 
-
-# define a predict function as an endpoint 
-@app.route("/predict", methods=["GET","POST"])
+# define a predict function as an endpoint
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
-    data = {"success": False}
-    # get the request parameters
-    params = flask.request.json
-    if (params == None):
-        params = flask.request.args
-    # if parameters are found, echo the msg parameter 
-    if (params != None):
-        data["response"] = params.get("msg")
-        data["success"] = True
-    # return a response in json format 
-    return flask.jsonify(data)
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            now = datetime.now()
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), file.filename.rsplit('.', 1)[1]))
+            file.save(filename)
+
+    # data = {"success": False}
+    # # get the request parameters
+    # params = flask.request.json
+    # if (params == None):
+    #     params = flask.request.args
+    # # if parameters are found, echo the msg parameter 
+    # if (params != None):
+    #     data["response"] = params.get("msg")
+    #     data["success"] = True
+    # # return a response in json format 
+    # return flask.jsonify(data)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 # start the flask app, allow remote connections
 app.run(host='0.0.0.0', port="8000")
